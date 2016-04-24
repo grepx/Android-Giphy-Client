d273f85c70bba3a432b37703aedb3ae8c3479dae
Greg Pearce <gregrpearce@gmail.com>
2016-04-24 10:37:09 +0100

    Delete unused MainView interface.

--- app/src/main/kotlin/gregpearce/gifhub/di/ViewComponent.kt
+++ app/src/main/kotlin/gregpearce/gifhub/di/ViewComponent.kt
@@ -3,7 +3,7 @@ package gregpearce.gifhub.di
 import dagger.Component
 import gregpearce.gifhub.app.MainApplication
 import gregpearce.gifhub.view.MainActivity
-import gregpearce.gifhub.view.MainViewImp
+import gregpearce.gifhub.view.MainView
 import javax.inject.Singleton
 
 @ViewScope
@@ -13,5 +13,5 @@ import javax.inject.Singleton
 )
 interface ViewComponent {
     fun inject(mainActivity: MainActivity)
-    fun inject(mainView: MainViewImp)
+    fun inject(mainView: MainView)
 }
